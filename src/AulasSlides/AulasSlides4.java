9dbb56123fb30ecf3c9be02027afee38abf414c5
Gabriel Temóteo da Silva Damião <gabrieltsd1106@gmail.com>
2019-05-20 21:21:11 -0300

    Exercicio aula slide

---
+++ src/AulasSlides/AulasSlides4.java
@@ -0,0 +1,42 @@
+package AulasSlides;
+/*
+ * @author: Gabrieltsd
+ */
+import java.util.Scanner;
+
+      public class AulasSlides4 
+{   
+      private String nome;
+      private String descricao;
+      private int codigo;
+      
+      public AulasSlides4() {}
+      
+      public void setNome(String nm)
+      {
+        nome = nm;
+      }
+      public String getNome()
+      {
+        return nome;
+      }
+      
+      public void setDescricao(String des)
+      {
+        descricao = des;
+      }
+      public String getDescriacao()
+      {
+        return descricao;
+      }
+      
+      public void setCodigo(int cod)
+      {
+        codigo = cod;
+      }
+      public int getCodigo()
+      {
+        return codigo;
+      }
+      
+}
